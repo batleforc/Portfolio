41fc7a60a2c1269057f56a75500d807fadf632d7
Maxime <maxleriche.60@gmail.com>
2020-07-01 03:47:02 +0200

    ajout du container aux network weebo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker container stop portfolio
 docker container rm portfolio
 docker build -t batlefroc/portfolio .
-docker run -p 8080:8080 -d --name portfolio batlefroc/portfolio 
+docker run -p 8080:8080 -d --network weebo --name portfolio batlefroc/portfolio 
 sleep 2s
 docker logs portfolio
