9ec6e1b46d498a997e8279915b31020237e71c18
Maxime <maxleriche.60@gmail.com>
2020-07-01 03:25:30 +0200

    End Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t batlefroc/portfolio .
-docker run -p 8080:8080 -d -name portfolio batlefroc/portfolio 
-
+docker run -p 8080:8080 -d --name portfolio batlefroc/portfolio 
+sleep 2s
 docker logs portfolio
