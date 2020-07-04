9491e322cede0a89d68dcf96259ef8a3709f1ad6
Maxime <maxleriche.60@gmail.com>
2020-07-04 16:15:39 +0200

    Eviter l'overflox des projet box

--- src/Rapp/projet.js
+++ src/Rapp/projet.js
@@ -30,7 +30,7 @@ class Projet extends React.Component{
                  <div className="projWrapper">
                    <div className="projGrid">
                    {this.state.Projet.map((value,index)=>{
-                    var todisplay = value.img==""?<FontAwesomeIcon icon={['fas','lightbulb']} size="10x" style={{display:"block",margin:"auto"}}/>:<img src={value.img}/>
+                    var todisplay = value.img==""?<FontAwesomeIcon icon={['fas','lightbulb']} size="5x" style={{display:"block",margin:"auto"}}/>:<img src={value.img}/>
                    return(<div key={value.slug} className="projItem" style={{display:"inline-block",background:'#222',margin:"2.5%",padding:"8px",height:"fit-content"}}><a href={"/projet/"+value.slug}>
                      {todisplay}
                      <p style={{textAlign:"center"}}>{value.name}</p>
