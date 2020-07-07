20fe3b2572ca37576343020ad7ffd5926cd4f84a
Maxime <maxleriche.60@gmail.com>
2020-07-07 19:10:12 +0200

    Projet => Faire une page de rendu projet propre

--- src/Rapp/DisplayProjet.js
+++ src/Rapp/DisplayProjet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Profil from './ResumeData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import parse from 'html-react-parser';
+import UrlPreview from './urlpreview';
 
 class DisplayProjet extends React.Component{
   constructor(props){
@@ -27,17 +28,43 @@ class DisplayProjet extends React.Component{
     render(){
         if(this.state.Projet[0]!=undefined){
             var todisplay = this.state.Projet[0].img==""?<FontAwesomeIcon icon={['fas','lightbulb']} size="10x" style={{display:"block",margin:"auto"}}/>:<img src={this.state.Projet[0].img}/>
-            console.log(this.state.Projet[0])
         }
-       
        return( <div>
-        <header className="w3-container w3-padding-32 w3-center w3-black" id="home">
         {todisplay}
-            <h2 className="w3-jumbo"><span className="w3-hide-small"></span> Projet  {this.state.Projet[0]!=undefined?this.state.Projet[0].name:this.props.slug}</h2>
-         </header>
-          {parse(this.state.Projet[0]!=undefined?this.state.Projet[0].description:"")}
-         <h3><a href={this.state.Projet[0]!=undefined?this.state.Projet[0].url:""}>Ce projet est accessible ici et est "{this.state.Projet[0]!=undefined?this.state.Projet[0].status:""}"</a></h3>
-         
+        <div style={{textAlign:"left"}} className="w3-padding-64 w3-content">
+        {this.state.Projet[0]&&this.state.Projet[0].data.blocks.map(function(value,index){
+          var render
+          if(value.type=="header"){
+             render =value.data.level==1?<h1 key={value.data.text}>{value.data.text}</h1>:value.data.level==2?<h2 key={value.data.text}>{value.data.text}</h2>:value.data.level==3?<h3 key={value.data.text}>{value.data.text}</h3>:value.data.level==4?<h4 key={value.data.text}>{value.data.text}</h4>:<h5 key={value.data.text}>{value.data.level}</h5>;
+          }
+          else if (value.type=="list"){
+            render=<ul key={value.data.style}>{value.data.items.map(function(item){
+              return <li key={item}>{parse(item)}</li>
+            })}</ul>
+          }
+          else if(value.type=="linkTool"){
+            render=<UrlPreview key={value.data} info={value.data} />
+          }
+          else if(value.type=="paragraph"){
+            render = <p key={value.data.text}>{value.data.text}</p>
+          }
+          else if(value.type=="table"){
+            render=(
+              <table key={value.type} style={{width:"100%",backgroundColor:"white",color:"black",borderCollapse:"collapse"}}>
+                <tbody>
+                  {value.data.content.map(function(row){
+                    return <tr  key={row}>{row.map(function(col){
+                      return(<td style={{border:"thin solid black"}} key={col}>{col}</td>)
+                    })}</tr>
+                  })}
+                </tbody>
+              </table>
+            )
+          }
+          return(render)
+        })}
+        </div>
+
         </div>)
     }
 }
--- src/Rapp/urlpreview.js
+++ src/Rapp/urlpreview.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 function UrlPreview(props) {
-    console.log(props)
-    if(!props.info) return(<div></div>)
+    if(!props.info) return(<div key={"urlpreviewkey"}></div>)
     return(
-    <div className="cdx-block" style={{textAlign:"left"}}>
-        <div className="link-tool">
-            <a className="link-tool__content link-tool__content--rendered" target="_blank" rel="nofollow noindex noreferrer" href={props.info.link?props.info.link:""}>
-                <div className="link-tool__image" style={{backgroundImage:"url(\""+props.info.meta.image.url+"\")"}}></div>
-                <div className="link-tool__title">{props.info.meta.title}</div>
-                <p className="link-tool__description">{props.info.meta.description}</p>
-                <span className="link-tool__anchor">{props.info.meta.domaine}</span>
+    <div className="cdx-block" style={{textAlign:"left",background:"grey"}} key={props.info.title+props.info.domaine}>
+        <div className="link-tool" key={props.info.meta.url}>
+            <a className="link-tool__content link-tool__content--rendered" key={props.info.meta.url+"link"} target="_blank" rel="nofollow noindex noreferrer" href={props.info.meta.url?props.info.meta.url:""}>
+                <div className="link-tool__image" key={props.info.meta.image.url} style={{backgroundImage:"url(\""+props.info.meta.image.url+"\")"}}></div>
+                <div className="link-tool__title" key={props.info.meta.title}>{props.info.meta.title}</div>
+                <p className="link-tool__description" key={props.info.meta.description}>{props.info.meta.description}</p>
+                <span className="link-tool__anchor" key={props.info.meta.domaine}>{props.info.meta.domaine}</span>
             </a>
         </div>
     </div>)
