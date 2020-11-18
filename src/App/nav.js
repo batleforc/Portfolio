61046d59411c550b82bf6500c743a0529aae16bc
Maxime Leriche <maxleriche.60@gmail.Com>
2020-11-18 13:06:24 +0100

    Update pour smooth scrolling

--- src/App/nav.js
+++ src/App/nav.js
@@ -7,29 +7,36 @@ class Nav extends React.Component{
   arewehome(){
     return window.location.pathname==="/"?true:false;
   }
+  Scroll=(event) =>{
+    event.preventDefault()
+    this.props.Click(event)
+    document.querySelector(event.currentTarget.getAttribute("href")).scrollIntoView({
+      behavior: 'smooth'
+  });
+  }
     render(){
         //<img src="/w3images/avatar_smoke.jpg" style={{width:"100%"}} />
         return(
             <div>
             <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
             <img src={logo} style={{width:"100%"}} alt="Logo du projet" />
-            <a href={this.arewehome()?"#":"/"} name="home" key="/" onClick={this.props.Click} className={this.props.where==="home"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}> 
+            <a href={this.arewehome()?"#main":"/"} name="home" key="/" onClick={this.Scroll} className={this.props.where==="home"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}> 
               <FontAwesomeIcon className="w3-xxlarge" icon={["fas","home"]}/>
               <p>HOME</p>
             </a>
-            {this.arewehome()&&<a href="#about" name="about" key="about" onClick={this.props.Click} className={this.props.where==="about"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
+            {this.arewehome()&&<a href="#about" name="about" key="about" onClick={this.Scroll} className={this.props.where==="about"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
                 <FontAwesomeIcon className="w3-xxlarge" icon={["fas","user"]}/>
                 <p>ABOUT</p>
               </a>}
-              {this.arewehome()&&<a href="#projets" name="projets" onClick={this.props.Click} key="projets"className={this.props.where==="projets"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
+              {this.arewehome()&&<a href="#projets" name="projets" onClick={this.Scroll} key="projets"className={this.props.where==="projets"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
                 <FontAwesomeIcon className="w3-xxlarge" icon={['fas',"cogs"]}/>
                 <p>PROJET</p>
               </a>}
-              {this.arewehome()&&<a href="#notif" name="notif" onClick={this.props.Click} key="notif" className={this.props.where==="notif"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
+              {this.arewehome()&&<a href="#notif" name="notif" onClick={this.Scroll} key="notif" className={this.props.where==="notif"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
               <FontAwesomeIcon className="w3-xxlarge" icon={['fas','bell']} />
                 <p>NOTIF</p>
               </a>}
-              {this.arewehome()&&<a href="#contact" name="contact" onClick={this.props.Click} key="contact" className={this.props.where==="contact"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
+              {this.arewehome()&&<a href="#contact" name="contact" onClick={this.Scroll} key="contact" className={this.props.where==="contact"?  "w3-bar-item w3-button w3-padding-large w3-black":"w3-bar-item w3-button w3-padding-large w3-hover-black"}>
                 <FontAwesomeIcon  className="w3-xxlarge" icon={["fas","envelope"]}/>
                 <p>CONTACT</p>
               </a>}
@@ -43,10 +50,10 @@ class Nav extends React.Component{
             </nav>
             <div className="w3-bottom w3-hide-large w3-hide-medium" id="myNavbar" style={{right:"0",left:"0"}}>
               <div className="w3-bar w3-grey w3-opacity-min w3-hover-opacity-off w3-center w3-small mobilenavbar">
-              <a href={this.arewehome()?"#":"/"} name="home" onClick={this.props.Click} className={this.props.where==="home"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon  icon={["fas","home"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>HOME</p></a>
-              {this.arewehome()&&<a href="#about" name="about" onClick={this.props.Click} className={this.props.where==="about"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}> <FontAwesomeIcon  icon={["fas","user"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>ABOUT</p></a>}
-              {this.arewehome()&& <a href="#projets" name="projets" onClick={this.props.Click} className={this.props.where==="projets"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon icon={['fas',"cogs"]} /><p style={{marginTop:"10px",marginBottom:"10px"}}>PROJET</p></a>}
-              {this.arewehome()&&<a href="#contact" name="contact" onClick={this.props.Click} className={this.props.where==="contact"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon  icon={["fas","envelope"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>CONTACT</p></a>}
+              <a href={this.arewehome()?"#main":"/"} name="home" onClick={this.Scroll} className={this.props.where==="home"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon  icon={["fas","home"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>HOME</p></a>
+              {this.arewehome()&&<a href="#about" name="about" onClick={this.Scroll} className={this.props.where==="about"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}> <FontAwesomeIcon  icon={["fas","user"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>ABOUT</p></a>}
+              {this.arewehome()&& <a href="#projets" name="projets" onClick={this.Scroll} className={this.props.where==="projets"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon icon={['fas',"cogs"]} /><p style={{marginTop:"10px",marginBottom:"10px"}}>PROJET</p></a>}
+              {this.arewehome()&&<a href="#contact" name="contact" onClick={this.Scroll} className={this.props.where==="contact"?"w3-bar-item w3-button w3-dark-grey":"w3-bar-item w3-button"} style={{width:"% !important",marginTop:"0px",marginBottom:"0px",paddingBottom:"2px"}}><FontAwesomeIcon  icon={["fas","envelope"]}/><p style={{marginTop:"10px",marginBottom:"10px"}}>CONTACT</p></a>}
               </div>
             </div>
             </div>
