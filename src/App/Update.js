bd4783c58b2e41405c7e3f6adf0b0e73774b3534
Maxime Leriche <maxleriche.60@gmail.com>
2021-01-16 16:28:56 +0100

    [fix] Correction CI + retrait import innutile

--- src/App/Update.js
+++ src/App/Update.js
@@ -1,5 +1,4 @@
 import React, {useState,useEffect} from 'react'
-import * as serviceWorker from '../serviceWorkerRegistration'
 
 export default function Update() {
     const [ShowSnackBar,setShowSnackBar] = useState(false)
