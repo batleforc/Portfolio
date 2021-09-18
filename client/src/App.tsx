c16618a092037fd47bddcecc1c9d5235ff49750e
Maxime Leriche <maxleriche.60@gmail.com>
2021-09-19 01:07:41 +0200

    Init Client Project

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 ">
+        Hello
+        <br />
+        You
+      </span>
+    </div>
+  );
+}
+
+export default App;
