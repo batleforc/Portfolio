7c76f16565eec24430bfc0b625612424605cfb29
Maxime Leriche <maxleriche.60@gmail.com>
2021-09-26 19:02:27 +0200

    [add] Ajout d'une partie About et Tech

--- client/src/propertie.js
+++ client/src/propertie.js
@@ -27,6 +27,70 @@ const properties = {
       link: "https://www.linkedin.com/in/maxime-leriche-weebo/",
     },
   ],
+  about: [
+    "Un étudiant en Master Architecte Logicielle. Entre le développeur Back et Front saupoudré d'un fan d'Infra.",
+  ],
+  tech: [
+    {
+      Label: "Core",
+      Icon: [
+        { icon: "github", label: "Exemple" },
+        { icon: "github", label: "Exemple" },
+      ],
+      categorie: [
+        {
+          Label: "Language",
+          List: ["C#", "C", "C++", "Php", "Node", "Python", "Java"],
+          Horizontal: false,
+        },
+      ],
+    },
+    {
+      Label: "Front",
+      Icon: [{ icon: "github", label: "Exemple" }],
+      categorie: [
+        {
+          Label: "Language",
+          List: ["JS", "ReactJS", "VueJS"],
+          Horizontal: false,
+        },
+      ],
+    },
+    {
+      Label: "Infra",
+      Icon: [{ icon: "github", label: "Exemple" }],
+      categorie: [
+        {
+          Label: "Language",
+          List: ["Heroku", "Docker", "Kubernetes", "Vm", "VPS"],
+          Horizontal: false,
+        },
+        {
+          Label: "Kubernetes",
+          List: [
+            "Microk8s",
+            "Keycloak",
+            "Gitea",
+            "Harbor",
+            "EclipseChe",
+            "Concourse",
+          ],
+          Horizontal: false,
+        },
+      ],
+    },
+    {
+      Label: "Base de donnée",
+      Icon: [{ icon: "github", label: "Exemple" }],
+      categorie: [
+        {
+          Label: "Gérer",
+          List: ["MariaDB", "MongoDB", "PostgreSQL"],
+          Horizontal: false,
+        },
+      ],
+    },
+  ],
 };
 
 export default properties;
